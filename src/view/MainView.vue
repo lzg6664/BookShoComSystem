a47b5db0fd21a93761676f5ba95a766a65ba7420
hgsyxw <3025138972@qq.com>
2024-06-16 11:21:19 +0800

    1

--- src/view/MainView.vue
+++ src/view/MainView.vue
@@ -3,6 +3,7 @@
         
         <!-- Your content goes here -->
          1321321
+         1321321
         <BookCatogory />
         <div class="book-cotainer">
             <BookShowCard v-for="(book,key) in books" :book="book" :key="key"  />
